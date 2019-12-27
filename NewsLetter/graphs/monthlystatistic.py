e1839d6e5575a887623598e7d2ea0c4b47b0e382
Hosely5 <tappannagari@gmail.com>
2019-12-26 16:31:31 -0800

    Added different source of facts

--- NewsLetter/graphs/monthlystatistic.py
+++ NewsLetter/graphs/monthlystatistic.py
@@ -2,17 +2,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import string
 from WordScrape import getword
+from FactScrape import scrapelen
 # Fixing random state for reproducibility
 
 
-x = getword(2019,12,25)
-print(x)
+days = list(range(1,3))
 
+daysx = []
+numevents = []
+
+for i in days:
+	daysx.append(i)
+	numevents.append(scrapelen("december",str(i)))
 
 # test = string.ascii_letters
 # test2 = test[0:9]
-# x = [1,2,3,4,5,6,7,8,10]
-# y = [3,4,5,6,8,10,11,10,11]
+# x = ["test","test2"] #,3,4,5,6,7,8,10]
+# y = [3,4]
 
 
 # xy = (1,3)
@@ -24,3 +30,6 @@ print(x)
 # 	plt.annotate(k,xy)
 
 # plt.show()
+
+plt.hist(daysx,bins=5)
+plt.show()
