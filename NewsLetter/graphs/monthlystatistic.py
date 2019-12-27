8977140f24e39dde3fe67831de6139217a78069c
Hosely5 <tappannagari@gmail.com>
2019-12-26 23:44:06 -0800

    Fixed scatter plot

--- NewsLetter/graphs/monthlystatistic.py
+++ NewsLetter/graphs/monthlystatistic.py
@@ -2,34 +2,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import string
 from WordScrape import getword
-from FactScrape import scrapelen
+from FactScrape import scrapelen,numfacts
 # Fixing random state for reproducibility
 
 
-days = list(range(1,3))
+days = list(range(1,2))
 
 daysx = []
 numevents = []
 
 for i in days:
 	daysx.append(i)
-	numevents.append(scrapelen("december",str(i)))
-
-# test = string.ascii_letters
-# test2 = test[0:9]
-# x = ["test","test2"] #,3,4,5,6,7,8,10]
-# y = [3,4]
+	numevents.append(numfacts("December",str(i)))
 
 
-# xy = (1,3)
+plt.scatter(daysx, numevents)
 
-# plt.scatter(x,y)
+for i,e in zip(daysx, numevents):
+	xy = (i,e)
+	plt.annotate("December " + str(i), xy)
 
-# for k,i,e in zip(test2,x,y):
-# 	xy = (i,e)
-# 	plt.annotate(k,xy)
 
-# plt.show()
-
-plt.hist(daysx,bins=5)
 plt.show()
