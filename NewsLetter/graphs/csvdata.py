887fdd84ca87c9588f8f53a82529eae526c1edc0
Hosely5 <tappannagari@gmail.com>
2020-01-05 18:21:34 -0800

    More efficient plotting

--- NewsLetter/graphs/csvdata.py
+++ NewsLetter/graphs/csvdata.py
@@ -4,23 +4,29 @@ from FactScrape import getfact, scrapelen, numfacts
 def gevents(month, day):
 	return numfacts(month, str(day))
 
+def totalevents(month):
+	days = range(1,3)
+	total = []
+	for i in days:
+		total.append(numfacts(month, str(i)))
+	return sum(total)
+
+	
 # days = range(1,3)
 # x = gevents("December",days)
 # print(x)
 
-def foo():
-	return ["December"]
 
 #Make into function
-with open('asd.csv', mode='w', newline='') as filedata:
-	maintest = csv.writer(filedata, delimiter=',')
-	maintest.writerow(["Month", "Day", "Events"])
-	days = range(1,10)
-	#example inputs
-	months = ["January", "March"]
-	for i in months:
-		for e in days:
-			maintest.writerow([i, e, numfacts(i,str(e))])
+# with open('asd.csv', mode='w', newline='') as filedata:
+# 	maintest = csv.writer(filedata, delimiter=',')
+# 	maintest.writerow(["Month", "Day", "Events"])
+# 	days = range(1,10)
+# 	#example inputs
+# 	months = ["January", "March"]
+# 	for i in months:
+# 		for e in days:
+# 			maintest.writerow([i, e, numfacts(i,str(e))])
 
 
 #Test for proper format
--- NewsLetter/graphs/monthlystatistic.py
+++ NewsLetter/graphs/monthlystatistic.py
@@ -7,7 +7,7 @@ from FactScrape import scrapelen,numfacts
 
 
 days = list(range(1,3))
-
+non_numerical = ["December", "March"]
 
 def gevents(month, maindays):
 	"""Return a list of events for given month and list of days"""
@@ -22,49 +22,36 @@ def gevents(month, maindays):
 		numevents.append(numfacts(month,str(i)))
 
 	return zip(daysx,numevents)
-# print(gevents("December", days))
-# print(gevents("March", days))
 
 def graphevents(graph, month, maindays, plotcolor="red"):
+	"""Annotate plotted points on graph"""
 	for i,e in gevents(month, maindays):
 		graph.scatter(i, e, color=plotcolor, label=month)
 		xy = (i,e)
 		# graph.annotate("Dec. " + str(i), xy, wrap=True)
 
+def plot_scatter(graph, month, data, color="red"):
+	plot_data = gevents(month, data)
+	x, y = zip(*plot_data)
+	result = graph.scatter(x,y, label=month)
+	return result
 
-# x = [1,2,3,4,5,6]
-# y = [3,4,1,2,8,10]
+def plot_bar(graph, month, data, color="blue"):
+	plot_data = gevents(month, data)
+	x, y = zip(*plot_data)
+	del x
+	const_x = [month] * len(y)
+	result = graph.bar(const_x,sum(y), label=month)
+	plt.annotate(str(sum(y)), (const_x, sum(y)))
+	# graph.annotate()
+	return result
 
-# # plt.scatter(x, y, color="blue") Organize points based on month, based on color
+# testdata = gevents("December", days)
+# x, y = zip(*testdata)
+# print(x,y)
 
 
 
-# for i,e in gevents("December", days):
-# 	plt.scatter(i, e, color="red")
-# 	xy = (i,e)
-# 	plt.annotate("Dec. " + str(i), xy, wrap=True)
-
-# for i,e in gevents("March", days):
-# 	plt.scatter(i, e, color="blue")
-# 	xy = (i,e)
-# 	plt.annotate("Mar. " + str(i), xy, wrap=True)
-
-k = gevents("December", days)
-x, y = zip(*k)
-print(y)
-
-#faster compile time
 fig, (ax) = plt.subplots(1,1)
-ax.scatter(x,y)
 
-# graphevents(ax2, "March", days, "blue")
-
-# ax.plot()
-
-# plt.title('Days')
-# ax.set_title('testero')
-# ax2.set_title('testero - 2')
-
-
-# plt.tight_layout()
 plt.show()
